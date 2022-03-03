47be7d059e1f686d5b3e2a821655d6beeb3053bd
cemakay <cema0092@stud.kea.dk>
2022-03-03 20:42:50 +0100

    first commit

---
+++ src/com/company/Sieve.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Sieve {
+
+    public static void main(String[] args) {
+        System.out.println("This program tells you all");
+        System.out.println("prime numbers up to a given maximum number.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type a maximum number.");
+        int max = scanner.nextInt();
+
+        List<Integer> primes = sieve(max);
+        System.out.println("Prime numbers up to " + max);
+        System.out.println(primes);
+
+    }
+    public static List<Integer> sieve(int max){
+        List<Integer> primes = new LinkedList<>();
+
+        List<Integer> numers = new LinkedList<>(); // add all numbers from 2 to max to the list
+        for (int i = 2; i <= max ; i++) {
+            numers.add(i);
+        }
+
+        while (!numers.isEmpty()){
+            int front = numers.remove(0); // remove a prime number from the front of the list
+            primes.add(front);
+
+            Iterator<Integer> itr = numers.iterator(); // remove all multiples of this prime number
+            while (itr.hasNext()){
+                int current = itr.next();
+                if (current % front == 0){
+                    itr.remove();
+                }
+            }
+        }
+        return primes;
+    }
+}
